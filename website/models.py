5a85c9803fbc39d50017220e5d847d7816ab9e24
Rossallen86 <rosshuberty@gmail.com>
2018-11-26 09:20:49 -0600

    course model unit tests

--- website/models.py
+++ website/models.py
@@ -1,25 +1,27 @@
 from django.db import models
+from ta_app.course_interface import ICourse
+
 
 
 class Account(models.Model):
     user = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     role = models.CharField(max_length=17)
-    street_address = models.CharField(max_length=35)
-    email_address = models.EmailField(max_length=30)
-    phone_number = models.CharField(max_length=12)
+    street_address = models.CharField(max_length=35, null=True)
+    email_address = models.EmailField(max_length=30, null=True)
+    phone_number = models.CharField(max_length=12, null=True)
 
 
 class Course(models.Model):
     name = models.CharField(max_length=20)
     section = models.CharField(max_length=3)
-    """days_of_week = []"""
+    days_of_week = models.CharField(max_length=8)
     start_time = models.CharField(max_length=5)
     end_time = models.CharField(max_length=3)
-    instructor = models.ForeignKey(Account, on_delete=models.CASCADE)
-    """tas = []"""
-    lab = models.CharField(max_length=3)
-    """lab_sections = []"""
+    instructor = models.ForeignKey(Account, related_name="instructor", on_delete=models.CASCADE, null=True)
+    tas = models.ManyToManyField(Account, related_name="tas", blank=True)
+    lab = models.CharField(max_length=3, null=True)
+    lab_sections = models.CharField(max_length=3, null=True)
 
 
 
