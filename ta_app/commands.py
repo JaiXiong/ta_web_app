c8c81570dbc686e734cff8d19cf5a2cb4644ca38
CEASar37 <connel45@uwm.edu>
2018-11-26 17:46:45 -0600

    added test cases and implementation

---
+++ ta_app/commands.py
@@ -0,0 +1,163 @@
+from ta_app.commands_interface import CommandsInterface
+import ta_app.wsgi
+from website.models import Account, Course
+
+
+class Commands(CommandsInterface):
+    def __init__(self):
+        self.current_user = Account()
+        self.command_list = {
+            "login": lambda *_: self.login(*_),
+            "help": lambda: self.help()
+        }
+
+    def login(self, username='', password=''):
+        if username == '' or password == '':
+            return 'login failed! bad username/ password'
+
+        existing_account = self.get_account(username)
+        if existing_account.user == username and existing_account.password == password:
+            self.current_user = existing_account
+            self.set_command_list()
+            return 'login successful...'
+        return 'login failed! bad username/ password'
+
+    def logout(self):
+        self.current_user = Account()
+        self.set_command_list()
+        return 'logout successful...'
+
+    def create_account(self, username='', password='', role=''):
+        if username == '' or password == '' or role == '':
+            return 'Failed to create account. Invalid or missing argument'
+
+        # check that current user has correct permissions to create account
+        cur_user_role = self.get_current_user().role
+        if cur_user_role != 'Supervisor' and cur_user_role != 'Administrator':
+            return 'Failed to create account. Insufficient permissions'
+        if cur_user_role == 'Administrator' and role == 'Supervisor':
+            return 'Failed to create account. Insufficient permissions'
+
+        valid_roles = ['Supervisor', 'Administrator', 'Instructor', 'TA']
+        role_is_valid = valid_roles.__contains__(role)
+        if not role_is_valid:
+            msg = 'Failed to create account. Invalid account type'
+            msg += '\nValid roles are ' + str(valid_roles)
+            return msg
+
+        existing_account = self.get_account(username)
+        if existing_account.id is not None:
+            return 'Failed to create account. User already exists'
+
+        new_account = Account(user=username, password=password, role=role)
+        new_account.save()
+        return 'Successfully created account'
+
+    def delete_account(self, user, password, role):
+        return ''
+
+    def edit_account(self, user):
+        return ''
+
+    def create_course(self, name, section, days, times, labs):
+        return ''
+
+    def assign_instructor(self, user, course):
+        return ''
+
+    def assign_ta_to_course(self, user, course):
+        return ''
+
+    def assign_ta_to_lab(self, user, course, lab):
+        return ''
+
+    def view_course_assignments(self):
+        return ''
+
+    def view_ta_assignments(self):
+        return ''
+
+    def read_contact_info(self):
+        return ''
+
+    def edit_contact_info(self):
+        return ''
+
+    def help(self):
+        commands = ''
+        for i in self.command_list.keys():
+            commands += i
+            commands += '\n'
+        return commands
+
+    def call_command(self, user_input):
+        params = user_input.split()
+        call = params[0]
+        args = params[1:len(params)]
+        return self.command_list.get(call, lambda *_: "ERROR: this is not an available command")(*args)
+
+    def get_current_user(self):
+        return self.current_user
+
+    def get_account(self, username=''):
+        accounts = list(Account.objects.filter(user=username))
+        if len(accounts) == 1:
+            return accounts.__getitem__(0)
+        return Account()
+
+    def set_command_list(self):
+        if self.current_user.id is None:
+            self.command_list = {
+                "login": lambda *_: self.login(*_),
+                "help": lambda: self.help()
+            }
+        role = self.current_user.role
+        if role == 'Supervisor':
+            self.command_list = {
+                "logout": lambda: self.logout(),
+                "create_account": lambda *_: self.create_account(*_),
+                "delete_account": lambda *_: self.delete_account(*_),
+                "edit_account": lambda *_: self.edit_account(*_),
+                "create_course": lambda *_: self.create_course(*_),
+                "assign_instructor": lambda *_: self.assign_instructor(*_),
+                "assign_ta_to_course": lambda *_: self.assign_ta_to_course(*_),
+                "assign_ta_to_lab": lambda *_: self.assign_ta_to_lab(*_),
+                "view_course_assignments": lambda *_: self.view_course_assignments(),
+                "view_ta_assignments": lambda *_: self.view_ta_assignments(),
+                "edit_contact_info": lambda *_: self.edit_contact_info(),
+                "read_contact_info": lambda *_: self.read_contact_info(),
+                "help": lambda *_: self.help()
+            }
+        if role == 'Administrator':
+            self.command_list = {
+                "logout": lambda: self.logout(),
+                "create_account": lambda *_: self.create_account(*_),
+                "delete_account": lambda *_: self.delete_account(*_),
+                "edit_account": lambda *_: self.edit_account(*_),
+                "create_course": lambda *_: self.create_course(*_),
+                "view_course_assignments": lambda *_: self.view_course_assignments(),
+                "view_ta_assignments": lambda *_: self.view_ta_assignments(),
+                "edit_contact_info": lambda *_: self.edit_contact_info(),
+                "read_contact_info": lambda *_: self.read_contact_info(),
+                "help": lambda *_: self.help()
+            }
+        if role == 'Instructor':
+            self.command_list = {
+                "logout": lambda: self.logout(),
+                "assign_ta_to_lab": lambda *_: self.assign_ta_to_lab(*_),
+                "view_course_assignments": lambda *_: self.view_course_assignments(),
+                "view_ta_assignments": lambda *_: self.view_ta_assignments(),
+                "edit_contact_info": lambda *_: self.edit_contact_info(),
+                "read_contact_info": lambda *_: self.read_contact_info(),
+                "help": lambda *_: self.help()
+            }
+        if role == 'TA':
+            self.command_list = {
+                "logout": lambda: self.logout(),
+                "view_ta_assignments": lambda *_: self.view_ta_assignments(),
+                "edit_contact_info": lambda *_: self.edit_contact_info(),
+                "read_contact_info": lambda *_: self.read_contact_info(),
+                "help": lambda *_: self.help()
+            }
+
+
--- ta_app/commands_interface.py
+++ ta_app/commands_interface.py
@@ -4,65 +4,65 @@ import abc
 class CommandsInterface(abc.ABC):
 
     @abc.abstractmethod
-    def login(self, user, password):
-        pass
+    def login(self, username, password):
+        return ''
 
     @abc.abstractmethod
     def logout(self):
-        pass
-
-    @abc.abstractmethod
-    def create_course(self, name, section, days, times, labs):
-        pass
+        return ''
 
     @abc.abstractmethod
     def create_account(self, user, password, role):
-        pass
+        return ''
 
     @abc.abstractmethod
     def delete_account(self, user, password, role):
-        pass
+        return ''
 
     @abc.abstractmethod
     def edit_account(self, user):
-        pass
+        return ''
+
+    @abc.abstractmethod
+    def create_course(self, name, section, days, times, labs):
+        return ''
 
     @abc.abstractmethod
     def assign_instructor(self, user, course):
-        pass
+        return ''
 
     @abc.abstractmethod
     def assign_ta_to_course(self, user, course):
-        pass
+        return ''
 
     @abc.abstractmethod
     def assign_ta_to_lab(self, user, course, lab):
-        pass
-
-    @abc.abstractmethod
-    def read_contact_info(self):
-        pass
-
-    @abc.abstractmethod
-    def edit_contact_info(self):
-        pass
+        return ''
 
     @abc.abstractmethod
     def view_course_assignments(self):
-        pass
+        return ''
 
     @abc.abstractmethod
     def view_ta_assignments(self):
-        pass
+        return ''
+
+    @abc.abstractmethod
+    def read_contact_info(self):
+        return ''
+
+    @abc.abstractmethod
+    def edit_contact_info(self):
+        return ''
 
     @abc.abstractmethod
     def help(self):
-        pass
+        return ''
+
+    @abc.abstractmethod
+    def call_command(self, user_input):
+        return ''
 
     @abc.abstractmethod
     def get_current_user(self):
-        pass
-
-    @abc.abstractmethod
-    def call_command(self, command):
-        pass
+        return ''
