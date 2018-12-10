e3a4c872706623e7ba6c7f8118a62f7b0ef9a252
Jai <xiongjai@gmail.com>
2018-12-10 16:21:01 -0600

    Basic working implementations:  Implemented login template  Implemented search template
     Implemented login view class
     Implemented search view class
    
    Login:
    1. Currently, the session works in login but there's still some inconsistencies.
    2. For the most part it's keep track of who's on what page. Will continue to debug this.
    
    Search:
    1. Currently, the search allows a supervisor or admin to access all 'data'.
    2. 'data', I've determined to be user information being displayed.
    3. 'data' is not editable or can be changed in search.
    4. Do we want class schedules or course assignments to show here?

--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,5 @@ from website import views
 urlpatterns = [
     url(r'^$', views.Home.as_view()),
     url(r'^login/$', views.Login.as_view()),
-    #url(r'^pizza/$', views.PizzaPageView.as_view()),
-    #url(r'^icecream/$', views.IcecreamPageView.as_view()),
-    #url(r'^purchase/$', views.PurchasecreamPageView.as_view())
+    url(r'^search/$', views.Search.as_view()),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from ta_app.commands import Commands
+from website.models import Account
 
 
 class Home(View):
@@ -16,12 +17,82 @@ class Home(View):
 
 class Login(View):
     ui = Commands()
-    template_name = "login.html"
-    def get_login(self, request):
-        print("inside getlogin")
-        return render(request, "login.html")
 
-    def post_login(self, request):
-        print("inside postlogin")
-        out = self.ui.call_command(request.POST["loginpage"])
-        return render(request, "login.html", {"out": out})
+    def get(self, request):
+        print("inside get login")
+        if "name" in request.session:
+            name = request.session["name"]
+        else:
+            name = "no user logged in, please log in"
+        return render(request, "website/login.html", {"name": name})
+
+    def post(self, request):
+        print("inside post login")
+        name = request.POST["username"]
+        password = request.POST["password"]
+        request.session["name"] = name
+        request.session["password"] = password
+        out = self.ui.login(name, password)
+        ctx = {"out": out, "name": name }
+        return render(request, "website/login.html", ctx)
+
+class Search(View):
+    ui = Commands()
+    def get(self, request):
+        print("inside get search")
+        if "name" in request.session:
+            name = request.session["name"]
+        else:
+            name = "no user logged in, please log in"
+        return render(request, "website/search.html", {"name": name})
+
+    def post(self, request):
+        print("inside post search")
+        user = ""
+        password = ""
+        role = ""
+        address = ""
+        email = ""
+        phone = ""
+        db = Account.objects.all()
+        searchuser = request.POST["searchuser"]
+        name = request.session["name"]
+        request.session["name"] = name
+        print("current: "+ name)
+        #current = self.ui.get_current_user()
+        #print(current)
+        current = self.ui.get_account(name)
+        print(current)
+        print(current.role)
+        if current.role == "Supervisor":
+            print("yay im supervisor")
+        if (current.role == "Administrator"):
+            print("yay im admin")
+        if current.role == "Supervisor" or current.role == "Administrator":
+            #print("Invalid permissions to view user")
+            #else:
+            print("did it make it here????")
+            searchthis = self.ui.get_account(searchuser)
+            print("trying to search: " + searchthis.user)
+            if db.filter(user=name).count() == 0:
+                return "Invalid search, try again"
+            else:
+                print("current information:")
+                name = request.session["name"]
+                print("user info:" + name)
+                user = searchthis.user
+                print(user)
+                password = searchthis.password
+                print(password)
+                role = searchthis.role
+                print(role)
+                address = searchthis.street_address
+                print(address)
+                email = searchthis.email_address
+                print(email)
+                phone = searchthis.phone_number
+                print(phone)
+        else:
+            print("Invalid permissions to view information")
+        ctx = {user, password, role, address, email, phone}
+        return render(request, "website/search.html", {"ctx": ctx}, {"name": name})
